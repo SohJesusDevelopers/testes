5b1484896152cda843c45f49174f2f0686a3039f
Swift <swift@imac-jobs-3.local>
2017-02-03 21:00:35 -0200

    materia ChatMultiPeer

--- ChatMultiPeer/ViewController.swift
+++ ChatMultiPeer/ViewController.swift
@@ -7,19 +7,162 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessionDelegate, UITextFieldDelegate {
+    
+    //MARK: Outlets
+    
+    @IBOutlet weak var textFieldMensagem: UITextField!
+    
+    @IBOutlet weak var textViewConversas: UITextView!
+    
+    //MARK: Propriedades
+    
+    var meuBuscador : MCBrowserViewController!
+    var meuAdvertiser : MCAdvertiserAssistant!
+    var minhaSecao : MCSession!
+    var meuPeerID : MCPeerID!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //MARK: Propriedades de textField
+        self.textFieldMensagem.delegate = self
+        
+        //Iniciando o Peer ID
+        
+        //comando para pegar informacoes do device (nome, modelo, versao, batteria)
+        self.meuPeerID = MCPeerID(displayName: UIDevice.current.name)
+        
+        //Iniciando uma Seção
+        self.minhaSecao = MCSession(peer: meuPeerID)
+        self.minhaSecao.delegate = self
+        
+        //Iniciando o Buscador
+        self.meuBuscador = MCBrowserViewController(serviceType: "chat", session: minhaSecao)
+        self.meuBuscador.delegate = self
+        
+        //Iniciando o Advertiser = avisa quem entrou e quem saiu
+        self.meuAdvertiser = MCAdvertiserAssistant(serviceType: "chat", discoveryInfo: nil, session: minhaSecao)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Actions
+    
+    @IBAction func btnBuscar(_ sender: UIButton) {
+        
+        self.present(meuBuscador, animated: true, completion: nil)
+        self.meuAdvertiser.start()
+        
     }
 
+    //MARK: Métodos de MCBrowserViewControllerDelegate
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        
+        print("Pressionou Done")
+        
+        self.meuBuscador.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        
+        print("Cancelou a busca")
+        
+        self.meuBuscador.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    //MARK: Métodos Próprios
+    func tratarMensagem(mensagem umaMensagem : String, peer umPeer : MCPeerID){
+        
+        var textoFinal = ""
+
+        if umPeer == self.meuPeerID {
+            
+            textoFinal = "\nEU: \(umaMensagem)"
+            
+        } else {
+            
+            textoFinal = "\n \(umPeer.displayName): \(umaMensagem)"
+            
+        }
+        
+        self.textViewConversas.text! += textoFinal
+    }
+    
+    func enviarMensagem() {
+        
+        let mensagem = self.textFieldMensagem.text!
+        
+        self.textFieldMensagem.text = ""
+        
+        let dado = mensagem.data(using: String.Encoding.utf8, allowLossyConversion: true)!
+        
+        do {
+            
+            try self.minhaSecao.send(dado, toPeers: minhaSecao.connectedPeers, with: MCSessionSendDataMode.reliable)
+            
+        } catch {}
+        
+        tratarMensagem(mensagem: mensagem, peer: meuPeerID)
+        
+    }
+    
+    //MARK: Métodos de UITextField Delegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if !(self.textFieldMensagem.text!.isEmpty){
+            
+            enviarMensagem()
+            
+        }
+        
+        textFieldMensagem.resignFirstResponder()
+        
+        return true
+        
+    }
+    
+    //MARK: MCSessionDelegate
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+        let mensagem = NSString(data: data, encoding: String.Encoding.utf8.rawValue) as? String
+        
+        DispatchQueue.main.async{
+            
+            self.tratarMensagem(mensagem: mensagem!, peer: peerID)
+            
+        }
+        
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+        //método vazio!
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+        //método vazio!
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL, withError error: Error?) {
+        
+        //método vazio!
+        
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        
+        //método vazio!
+        
+    }
 
 }
 
